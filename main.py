d05f1d8325e9d4b91e94e9742d502125563a4f38
Crisanto de los Santos <crisanto.delossantos@gmail.com>
2023-03-06 20:05:17 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from pathlib import Path
 
 app = Flask(__name__)
+path_name=str(Path.cwd())
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"+path_name+"/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
+class Post(db.Model):
+	__tablename__ = "posts"
+	id = db.Column(db.Integer, primary_key=True)
+	titulo = db.Column(db.String, nullable=False)
+	fecha = db.Column(db.DateTime, default=datetime.now)
+	texto = db.Column(db.String, nullable=False)
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route("/")
+def inicio():
+	posts = Post.query.order_by(Post.fecha.desc()).all() 
+	return render_template("inicio.html", posts=posts)
 
+@app.route("/agregar")
+def agregar():
+	return render_template("agregar.html")
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+@app.route("/crear", methods=["POST"])
+def crear_post():
+	titulo = request.form.get("titulo")
+	texto = request.form.get("texto")
+	post = Post(titulo=titulo, texto=texto)
+	db.session.add(post)
+	db.session.commit()
+	return redirect("/")
+
+@app.route("/borrar", methods=["POST"])
+def borrar():
+	post_id = request.form.get("post_id")
+	post = db.session.query(Post).filter(Post.id==post_id).first()
+	db.session.delete(post)
+	db.session.commit()
+	return redirect("/")
+
+if __name__ == "__main__":
+	app.run(debug=True)
